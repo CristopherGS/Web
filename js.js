b81e7671b16247deef2091cccb58fbdbee327eb3
CristopherGS <guerracristofer@gmail.com>
2021-03-23 20:27:51 -0600

    nuevo dise;o

---
+++ js.js
@@ -0,0 +1,6 @@
+const  $close = document.getElementById('close').content
+
+.addEventListener('click', fun = () => {    
+    $close = $close.innerHTML('')
+    console.log($close);
+})
---
+++ s/js.js
@@ -0,0 +1,32 @@
+// let resultados = []
+const $plantilla = document.getElementById('template').content,
+    $fragmento = document.createDocumentFragment(),
+    $contene = document.getElementById('container-fluid')
+
+async function API() {
+    const jason = await fetch('https://rickandmortyapi.com/api/character/?page=25')
+    const res = await jason.json()
+    const resultados = await res.results
+
+    resultados.forEach(ress => {
+
+        const copiar = $plantilla.cloneNode(true)
+
+        copiar.querySelector(".card-header").textContent = ress.name
+
+        copiar.querySelector(".img").src = ress.image
+
+        copiar.querySelector(".card-text").textContent = ress.species
+
+        $fragmento.appendChild(copiar)
+    });
+
+    $contene.appendChild($fragmento)
+
+
+}
+API()
+document.getElementById('btn').addEventListener('click', function() {
+    history.back()
+    console.log('entro');
+})
